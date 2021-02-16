80cea6a13113937d2527cdac217eb06a8bcd2fb7
Lisa McGerr <lisamarie.mcgerr@gmail.com>
2021-02-16 17:35:07 -0600

    committing frontend.js changes

---
+++ frontend.js
@@ -0,0 +1,31 @@
+function main(){
+    fetchName()
+}
+
+
+function fetchName(){
+    fetch("http://localhost:3000/destinations")
+    .then(resp => resp.json())
+    .then(destinations => {
+        destinations.forEach(function(destination){
+            renderDestinations(destination)
+        })
+    }) 
+}
+
+// change from P Tag
+function renderDestinations(destination){
+    const body = document.querySelector('body')
+    
+    const pTag = document.createElement('p')
+    pTag.className = 'destination-tag'
+    pTag.dataset.id = destination.id
+    pTag.innerText = destination.city
+
+    
+    
+    body.append(pTag)
+}
+
+
+main()
