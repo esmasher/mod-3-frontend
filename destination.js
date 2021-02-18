c4a5fe32074c6c0777659fa1f9437665008d7582
esmasher <esmasherr@gmail.com>
2021-02-18 14:06:05 -0600

    itinerary

--- destination.js
+++ destination.js
@@ -1,6 +1,9 @@
 
 function main(){
     fetchName()
+    restClick()
+    addNote()
+    createNewHotel()
 }
 
 
@@ -17,7 +20,7 @@ function fetchName(){
 // change from P Tag
 function renderDestinations(destination){
     const destinationBar = document.querySelector('#destination-bar')
-    console.log(destinationBar)
+    // console.log(destinationBar)
     const span = document.createElement('span')
     span.innerText = destination.city
     span.className = 'destination-span'
@@ -27,9 +30,10 @@ function renderDestinations(destination){
     span.addEventListener('click', function(e){
         if (e.target.className === 'destination-span'){
             const id = e.target.dataset.id
+            fetchRestaurants(id)
             // console.log(e.target)
             fetchDestination(id)
-            fetchRestaurants(id)
+            fetchItinerary(id)
         }
     })
 }
@@ -48,12 +52,10 @@ function showDestination(destination){
     h1.innerHTML = destination.city
     const image = document.querySelector('#image')
     image.setAttribute('src', destination.image)
-    console.log(destination.hotels)
+    // console.log(destination.hotels)
     //const ul = document.querySelector('#hotel')
 
     //ul.append(hotelNames(destination))
-
-
 
 
 
@@ -65,6 +67,9 @@ function showDestination(destination){
                 pTag.innerText = hotel.name
                 pTag.dataset.id = hotel.id
                 div.append(pTag)
+
+                renderItineraryHotel(hotel)
+
                 pTag.addEventListener('click', function(e){
                     //console.log(e.target)
                     //debugger
@@ -78,7 +83,7 @@ function showDestination(destination){
 
         hotelNames(destination)
 
-    console.log(destination.hotels[0])
+    // console.log(destination.hotels[0])
 
     // const h4Restaurant = document.querySelector('#restaurant')
     // h4Restaurant.innerText = `Restaurants: ${destination.restaurant}`
@@ -102,14 +107,9 @@ function fetchHotelDetails(id){
     })
 }
 
-}
 
-function fetchRestaurants(id){
 
-    fetch("http://localhost:3000/restaurants")
-    .then(resp => resp.json())
-    .then(data => console.log(data))
-    // if (id === res)
+
 function renderHotel(hotel){
     console.log(hotel)
     const destinationContainer = document.querySelector('.destination')
@@ -132,6 +132,219 @@ function renderHotel(hotel){
     displayComments(hotel)
     //const image = document.querySelector('#image')
     //image.setAttribute('src', destination.image)
+}
+
+function fetchRestaurants(id){
+    const p = document.createElement('p')
+
+    fetch("http://localhost:3000/restaurants")
+    .then(resp => resp.json())
+    .then(restaurants => restaurants.forEach(function(restaurant){
+        dest_id = parseInt(restaurant.destination_id)
+        if ( parseInt(id) === dest_id) {
+
+        const h4_rest = document.getElementById("restaurant")
+
+        p.setAttribute("class", "rest-details")
+        p.innerHTML = `Restaurants: ${restaurant.name}`
+        p.dataset.id = restaurant.id
+
+        h4_rest.append(p)
+        renderItineraryRestaurant(restaurant)
+
+        }
+    }))
+}
+
+function restClick(){
+    const h4_rest = document.getElementById("restaurant")
+
+    h4_rest.addEventListener("click", function(event){
+
+        console.log(event.target.dataset.id)
+        if(event.target.className === "rest-details"){
+            const id = event.target.dataset.id
+
+            fetch(`http://localhost:3000/restaurants/${id}`)
+            .then(resp => resp.json())
+            .then(restaurant => renderRestaurantDetails(restaurant))
+        }
+
+
+    })
+}
+
+function renderRestaurantDetails(restaurant){
+    const h4_rest = document.getElementById("restaurant")
+    const pName = document.createElement("p")
+    const pCuisine = document.createElement('p')
+    const pReview = document.createElement('p')
+
+    pName.innerHTML = `Restaurant Name: ${restaurant.name}`
+    pCuisine.innerHTML = `Restaurant Cuisine: ${restaurant.cuisine}`
+    pReview.innerHTML = `Restaurant review: ${restaurant.review}`
+
+    h4_rest.append(pName, pCuisine, pReview)
+
+}
+
+function fetchItinerary(id){
+    const div = document.querySelector(".itinerary")
+    const h3 = document.querySelector(".iti-details")
+    const city = document.createElement('p')
+
+
+    h3.innerHTML = ("")
+    // const hotel = document.createElement('p')
+
+    fetch("http://localhost:3000/itineraries")
+    .then(resp => resp.json())
+    .then(itineraries => itineraries.forEach(function(itinerary){
+        dest_id = parseInt(itinerary.destination_id)
+        if ( parseInt(id) === dest_id){
+            city.dataset.id = itinerary.destination_id
+            // hotel.dataset.id = itinerary.id
+            // h4.dataset.id = itinerary.id
+
+            city.innerHTML = `Itinerary for: ${itinerary.destination.city}`
+            // hotel.innerHTML = `Hotel: ${itinerary.destination.hotel}`
+            h3.append(city)
+
+        }
+
+    }))
+
+}
+
+function renderItineraryRestaurant(restaurant){
+    const div = document.querySelector(".itinerary")
+    const h4 = document.querySelector("#itinerary-restaurant")
+    // const h4Tag = document.createElement("h4")
+    h4.innerHTML = ("")
+    const rest = document.createElement('p')
+
+    rest.dataset.id = restaurant.id
+    rest.innerHTML = `Restaurant: ${restaurant.name}`
+
+    h4.append(rest)
+    // h4.append(h4Tag)
+}
+
+function renderItineraryHotel(hotel){
+    const h4Hotel = document.querySelector("#itinerary-hotel")
+    // const  = document.createElement("h4")
+    h4Hotel.innerHTML = ("")
+    const hotell = document.createElement('p')
+    hotell.dataset.id = hotel.id
+    hotell.innerHTML = `Hotel: ${hotel.name}`
+
+    h4Hotel.append(hotell)
+    deleteHotel(hotel, hotell)
+    // div.append(h4Hotel)
+}
+
+
+
+function addNote(){
+    const newNote = document.querySelector(".itinerary")
+    const noteTag = document.querySelector("#new-note-created")
+    const destinationId = document.querySelector(".iti-details")
+
+    h5 = document.createElement("h5")
+    h5.id = "note-details"
+
+
+
+      newNote.addEventListener("submit", function(event){
+        event.preventDefault()
+        console.log(destinationId.firstChild.dataset.id)
+        const noteNew = event.target["new-note"].value
+        console.log(noteNew)
+        const id = destinationId.firstChild.dataset.id
+        // const noteTag = (event.target.previousSibling.previousSibling.previousSibling.previousSibling)
+
+
+        const reqObj = {
+            method: 'PATCH',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              note: noteNew
+
+            })
+          }
+
+          fetch(`http://localhost:3000/itineraries/${id}`, reqObj)
+            .then(resp => resp.json())
+            .then(data => {
+                event.target.reset()
+                console.log(data)
+                const p = document.createElement('p')
+                p.innerText = `Notes: ${data.note}`
+                h5.append(p)
+                noteTag.append(h5)
+            })
+
+        })
+
+}
+
+function deleteHotel(hotel, hotell){
+    const button = document.createElement('BUTTON')
+    button.innerHTML= "Remove Hotel"
+    hotell.append(button)
+
+    const id = hotel.id
+
+    hotell.addEventListener("click", function(event){
+        const reqObj = {
+            method: 'DELETE'
+          }
+
+        fetch(`http://localhost:3000/hotels/${id}`, reqObj)
+          .then(resp => resp.json())
+          .then(data => {
+            event.target.parentNode.parentNode.remove()
+          })
+    })
+}
+
+function createNewHotel(){
+    const form = document.querySelector('#create-hotel')
+    const h4Hotel = document.querySelector("#new-hotel-created")
+    const destination = document.querySelector('.iti-details')
+
+    h4Hotel.innerHTML = ("")
+    const hotell = document.createElement('p')
+
+
+    form.addEventListener('submit', function(event){
+        event.preventDefault()
+
+
+        const newHotel = {
+            name: event.target['name'].value,
+            rating: event.target['rating'].value,
+            destination_id: destination.dataset.id,
+        }
+        form.reset()
+        const reqObj = {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newHotel)
+          }
+    fetch('http://localhost:3000/hotels', reqObj)
+    .then(resp => resp.json())
+    .then(hotel => {
+        hotell.dataset.id = hotel.id
+        hotell.innerHTML = `Hotel: ${hotel.name}`
+        h4Hotel.append(hotell)
+        deleteHotel(hotel, hotell)
+        })
+    })
 }
 
 main()
--- itinerary.js
+++ itinerary.js
@@ -1,69 +1,82 @@
-function main(){
-    fetchItinerary()
-    addNewNote()
-}
+// function main(){
+//     fetchItinerary()
+//     addNewNote()
+// }
+
+// function renderDestinations(destination){
+//     const span = document.querySelector('.destination')
+
+//     span.addEventListener('click', function(e){
+//         if (e.target.className === 'destination-span'){
+//             const id = e.target.dataset.id
+//             fetchItinerary(id)
+//         }
+//     })
+// }
+
+// // need id so that it doesn't render itineraries for each destination
+// //render will be tied to destination render so conditional where destination.id === destination.id
+// function fetchItinerary(id){
+//     fetch("http://localhost:3000/itineraries")
+//     .then(resp => resp.json())
+//     .then(itineraries => itineraries.forEach(function(itinerary){
+//         dest_id = parseInt(itinerary.destination_id)
+//         if ( parseInt(id) === dest_id){}
+//     // .then(itinerary => itinerary.forEach(function(itinerary){
+//     //     renderItinerary(itinerary)
+//     }))
+// }
+
+// function renderItinerary(itinerary){
+//     const div = document.querySelector(".itinerary")
+//     const city = document.createElement('p')
+//     const hotel = document.createElement('p')
+//     const restaurant = document.createElement('p')
+//     const attraction = document.createElement('p')
+
+//     city.dataset.id = itinerary.id
+//     hotel.dataset.id = itinerary.id
+//     restaurant.dataset.id = itinerary.id
+//     attraction.dataset.id = itinerary.id
+
+//     const id = itinerary.destination_id
+
+//     fetch(`http://localhost:3000/destinations/${id}`)
+//     .then(resp => resp.json())
+//     .then(destination => {
+//         city.innerHTML = `City: ${destination.city}`
+//         hotel.innerHTML = `Hotel: ${destination.hotel}`
+//         restaurant.innerHTML = `Restaurant: ${destination.restaurant}`
+//         attraction.innerHTML = `Attraction: ${destination.attraction}`
+//         // div.append(city, hotel, restaurant, attraction)
+//         // console.log(destination)
+//         div.append(hotel)
+//     })
+// }
+
+// //form id will be itinerary id
+
+// function addNewNote(){
+
+//   const newNote = document.querySelector("form")
+
+//   newNote.addEventListener("submit", function(event){
+//     event.preventDefault()
+
+//     const note = document.querySelector("#new-note")
+//     const div = document.querySelector(".itinerary")
+//     const p = document.createElement('p')
+
+//     p.innerHTML = note.value
+
+//     div.append(p)
+// //    console.log(event.target)
+
+//   })
+
+// }
 
 
-// need id so that it doesn't render itineraries for each destination
-//render will be tied to destination render so conditional where destination.id === destination.id
-function fetchItinerary(){
-    fetch("http://localhost:3000/itineraries")
-    .then(resp => resp.json())
-    .then(itinerary => itinerary.forEach(function(itinerary){
-        renderItinerary(itinerary)
-    }))
-}
-
-function renderItinerary(itinerary){
-    const div = document.querySelector(".itinerary")
-    const city = document.createElement('p')
-    const hotel = document.createElement('p')
-    const restaurant = document.createElement('p')
-    const attraction = document.createElement('p')
-
-    city.dataset.id = itinerary.id
-    hotel.dataset.id = itinerary.id
-    restaurant.dataset.id = itinerary.id
-    attraction.dataset.id = itinerary.id
-
-    const id = itinerary.destination_id
-
-    fetch(`http://localhost:3000/destinations/${id}`)
-    .then(resp => resp.json())
-    .then(destination => {
-        city.innerHTML = `City: ${destination.city}`
-        hotel.innerHTML = `Hotel: ${destination.hotel.hotel}`
-        restaurant.innerHTML = `Restaurant: ${destination.restaurant}`
-        attraction.innerHTML = `Attraction: ${destination.attraction}`
-        // div.append(city, hotel, restaurant, attraction)
-        // console.log(destination)
-        div.append(hotel)
-    })
-}
-
-//form id will be itinerary id
-
-function addNewNote(){
-
-  const newNote = document.querySelector("form")
-
-  newNote.addEventListener("submit", function(event){
-    event.preventDefault()
-
-    const note = document.querySelector("#new-note")
-    const div = document.querySelector(".itinerary")
-    const p = document.createElement('p')
-
-    p.innerHTML = note.value
-
-    div.append(p)
-//    console.log(event.target)
-
-  })
-
-}
 
 
-
-
-main()
+// main()
